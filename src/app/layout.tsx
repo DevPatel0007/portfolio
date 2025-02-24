79a6490ab5b5e9a7de1ecfc8a4cc0133c070efd4
DevPatel0007 <161621068+DevPatel0007@users.noreply.github.com>
2025-02-24 09:16:27 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,21 +24,9 @@ export async function generateMetadata() {
       siteName: `${person.firstName}'s Portfolio`,
       locale: "en_US",
       type: "website",
-      image :[
-      {url: "/images/avatar2.png"
-       width: 1200,
-	height: 630,
-	alt:"Dev J Patel's Profile Picture",
-	},
-		    
-	    ]
+      
     },
-	   twitter: {
-      card: "summary_large_image",
-      title: "Dev J Patel's Portfolio",
-      description: "Portfolio website showcasing my work as a PYTHON DEVELOPER/AI ML DEVELOPER",
-      images: ["/images/avatar2.png"], // Twitter preview image
-    },
+	   
     robots: {
       index: true,
       follow: true,
